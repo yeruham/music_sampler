73d5ac2c4ba3c9ca797442ea08d35948789f527a
YeruhamMendelson <y0583207549@gmail.com>
2025-11-25 11:14:02 +0200

    style of select instrument

--- client/src/components/InstrumentsControl.tsx
+++ client/src/components/InstrumentsControl.tsx
@@ -1,9 +1,17 @@
+import "./style/InstrumentsControl.css";
 import InstrumentsInput from "./InstrumentsInput";
 import type InstrumentsProps from "../interfaces/InstrumentsProps";
 
 function InstrumentsControl({ instruments, setInstrument }: InstrumentsProps) {
   // const instruments = ["a", "b", "c"];
-  return <InstrumentsInput instruments={instruments} setInstrument={setInstrument}></InstrumentsInput>;
+  return (
+    <div className="instruments-control">
+      <InstrumentsInput
+        instruments={instruments}
+        setInstrument={setInstrument}
+      ></InstrumentsInput>
+    </div>
+  );
 }
 
 export default InstrumentsControl;
--- client/src/components/InstrumentsInput.tsx
+++ client/src/components/InstrumentsInput.tsx
@@ -1,16 +1,24 @@
 import type InstrumentsProps from "../interfaces/InstrumentsProps";
 
-function InstrumentsInput({ instruments, setInstrument }: InstrumentsProps){
+function InstrumentsInput({ instruments, setInstrument }: InstrumentsProps) {
+  const changeInstrument = (instrument: string) => {
+    setInstrument(instrument);
+  };
 
-    const changeInstrument = (instrument: string) => {
-        setInstrument(instrument);
-    }
-
-    return (<select onChange={(e) => changeInstrument(e.target.value)}>
-        {instruments.map((instrument, index) => {
-            return (<option key={index}>{instrument}</option>)
-        })}
-    </select>)
+  return (
+    <select
+      className="select-instrument"
+      onChange={(e) => changeInstrument(e.target.value)}
+    >
+      {instruments.map((instrument, index) => {
+        return (
+          <option key={index} className="instrument-option">
+            {instrument}
+          </option>
+        );
+      })}
+    </select>
+  );
 }
 
 export default InstrumentsInput;
