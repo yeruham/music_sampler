378ffc0791eeee8e30065fb44a59eee456fe3e6b
YeruhamMendelson <y0583207549@gmail.com>
2025-11-25 18:12:36 +0200

    new objects of instruments & urls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,18 @@ import PlayController from "./components/PlayController";
 import InstrumentsControl from "./components/InstrumentsControl";
 
 function App() {
-  const violinUrls = {
-    C6: "piano/c6.mp3",
-    D6: "piano/d6.mp3",
-    E6: "piano/e6.mp3",
-    F6: "piano/f6.mp3",
-    G6: "piano/g6.mp3",
+  const accordionUrls = {
+    C5: "accordion/C5.wav",
+    D5: "accordion/D5.wav",
+    E5: "accordion/E5.wav",
+    F5: "accordion/F5.wav",
+    G5: "accordion/G5.wav",
+    A5: "accordion/A5.wav",
+    B5: "accordion/B5.wav",
   };
 
   const pianoUrls = {
-     C6: "piano/c6.mp3",
+    C6: "piano/c6.mp3",
     D6: "piano/d6.mp3",
     E6: "piano/e6.mp3",
     F6: "piano/f6.mp3",
@@ -22,19 +24,22 @@ function App() {
     B6: "piano/b6.mp3",
   };
 
-  const trumpetUrls = {
-    B6: "piano/b6.mp3",
-    C6: "piano/c6.mp3",
-    D6: "piano/d6.mp3",
-    F6: "piano/f6.mp3",
+  const guitarUrls = {
+    C2: "guitar/C2.flac",
+    D2: "guitar/D2.flac",
+    E2: "guitar/E2.flac",
+    F2: "guitar/F2.flac",
+    G2: "guitar/G2.flac",
+    A2: "guitar/A2.flac",
+    B2: "guitar/B2.flac",
   };
 
   const [urls, setUrls] = useState<{ [key: string]: string }>(pianoUrls);
 
   const instruments: { [key: string]: { [key: string]: string } } = {
     piano: pianoUrls,
-    violin: violinUrls,
-    trumpet: trumpetUrls,
+    accordion: accordionUrls,
+    guitar: guitarUrls,
   };
 
   const defultInstrument = "piano";
