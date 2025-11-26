c3e274abe4fa01809f2fba00f8b38a1e458d99ab
YeruhamMendelson <y0583207549@gmail.com>
2025-11-26 12:11:07 +0200

    basic folders for server. config/minio.ts for minioClient

---
+++ server/src/config/minio.ts
@@ -0,0 +1,18 @@
+import { Client } from "minio";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const endpoint =  process.env.MINIO_ENDPOINT || "localhost";
+let port; 
+try { port = Number(process.env.MINIO_PORT) } catch{ port = 9000 }
+const accessKey = process.env.MINIO_ACCESS_KEY || "minioadmin";
+const secretKey = process.env.MINIO_SECRET_KEY || "minioadmin";
+
+export const minioClient = new Client({
+  endPoint: endpoint,
+  port: port,
+  useSSL: false,
+  accessKey: accessKey,
+  secretKey: secretKey
+});
