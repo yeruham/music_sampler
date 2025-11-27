b1910d16315b71bd3370c017099dde30edcdbcfc
YeruhamMendelson <y0583207549@gmail.com>
2025-11-27 15:15:23 +0200

    arrange server calls and data loading in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,108 +2,67 @@ import "./App.css";
 import { useEffect, useRef, useState } from "react";
 import PlayController from "./components/PlayController";
 import InstrumentsControl from "./components/InstrumentsControl";
-import { getInstruments, getUrlsOfInstrument } from "./utils/fetchNotes"
+import { getInstruments, getUrlsOfInstrument } from "./utils/fetchUrlsNotes"
 
 
 function App() {
 
   const instruments = useRef<string[] | undefined>(undefined);
-  const urlsByInstruments: { [key: string]: { [key: string]: string } } = {};
-
-  useEffect(() => {
-  loadnotesInfo().then( () => {
-    if (instruments && instruments.current!.length > 0){
-      const instrument = instruments.current![0];
-      const urlsOfInstrument = urlsByInstruments[instrument];
-      setUrls(urlsOfInstrument);
-      setCurrentInstrument(instrument);
-    }
-  }
-  ).catch(
-
-  )
-  }, [])
-
-  const loadnotesInfo = async () => {
-    instruments.current = await getInstruments();
-    console.log(instruments);
-    for (let i = 0; i < instruments.current!.length; i++){
-      const instrument = instruments.current![i];
-      const urls = await getUrlsOfInstrument(instrument);
-      console.log(urls)
-      if (urls){
-        urlsByInstruments[instrument] = urls;
-      }
-    }
-    console.log(urlsByInstruments);
-  }
-
-
-
-
-  const [urls, setUrls] = useState<{ [key: string]: string }>();
-
+  const urlsByInstruments = useRef<{ [key: string]: { [key: string]: string } }>({});
+  const [notesUrls, setNotesUrls] = useState<{ [key: string]: string } | undefined>();
   const [currentInstrument, setCurrentInstrument] = useState<string | undefined>();
 
+  const loadInfo = async () => {
+    instruments.current = await getInstruments();
+    if (instruments.current){
+      for (let i = 0; i < instruments.current.length; i++){
+        const instrument = instruments.current![i];
+        const urls = await getUrlsOfInstrument(instrument);
+        if (urls){
+          urlsByInstruments.current[instrument] = urls;
+        }
+      }
+    }
+  }
+
   useEffect(() => {
-    if (currentInstrument && urlsByInstruments[currentInstrument]){
-      const urlsOfInstrument = urlsByInstruments[currentInstrument];
-      setUrls(urlsOfInstrument);
+    loadInfo().then( () => {
+      if (instruments.current && instruments.current.length > 0){
+        const instrument = instruments.current![0];
+        const notesUrls = urlsByInstruments.current[instrument];
+        setNotesUrls(notesUrls);
+        setCurrentInstrument(instrument);
+      }
+    }
+    ).catch( (err) => {
+      console.log("Error" + err);
+    })
+  }, [])
+
+  useEffect(() => {
+    if (currentInstrument && urlsByInstruments.current[currentInstrument]){
+      const notesUrls = urlsByInstruments.current[currentInstrument];
+      setNotesUrls(notesUrls);
     }
   }, [currentInstrument]);
 
 
   return (
     <>
+    {(!currentInstrument || !notesUrls) && <p>Loading music notes</p>}
+    { currentInstrument && notesUrls && 
+    <>
       <div className="head">
         <h1>music sampler</h1>
-      { currentInstrument && urls &&  <InstrumentsControl
+       <InstrumentsControl
           instruments={instruments.current!}
           setInstrument={setCurrentInstrument}
-        ></InstrumentsControl>}
+        ></InstrumentsControl>
       </div>
-      {currentInstrument &&  urls &&  <PlayController urls={urls!}></PlayController>}
+      <PlayController urls={notesUrls}></PlayController>
+      </>}
     </>
   );
 }
 
 export default App;
-
-
-
-  const accordionUrls = {
-    C5: "accordion/C5.wav",
-    D5: "accordion/D5.wav",
-    E5: "accordion/E5.wav",
-    F5: "accordion/F5.wav",
-    G5: "accordion/G5.wav",
-    A5: "accordion/A5.wav",
-    B5: "accordion/B5.wav",
-  };
-
-  const pianoUrls = {
-    C6: "piano/c6.mp3",
-    D6: "piano/d6.mp3",
-    E6: "piano/e6.mp3",
-    F6: "piano/f6.mp3",
-    G6: "piano/g6.mp3",
-    A6: "piano/a6.mp3",
-    B6: "piano/b6.mp3",
-  };
-
-  const guitarUrls = {
-    C2: "guitar/C2.flac",
-    D2: "guitar/D2.flac",
-    E2: "guitar/E2.flac",
-    F2: "guitar/F2.flac",
-    G2: "guitar/G2.flac",
-    A2: "guitar/A2.flac",
-    B2: "guitar/B2.flac",
-  };
-
-
-   const instruments: { [key: string]: { [key: string]: string } } = {
-    piano: pianoUrls,
-    accordion: accordionUrls,
-    guitar: guitarUrls,
-  };
---
+++ client/src/utils/fetchUrlsNotes.ts
@@ -0,0 +1,46 @@
+export const prefixUrl = "http://localhost:3000";
+export const basicUrl = `${prefixUrl}/instruments`;
+
+
+export async function getInstruments(): Promise<string[] | undefined>{
+    try{
+      const response = await fetch(basicUrl);
+      if (response.ok){
+        const instruments = await response.json();
+        return instruments;
+      }
+    }
+    catch (err){
+      console.log(err)
+    }
+}
+
+
+export async function getUrlsOfInstrument(instrument: string, addPrefix: boolean=true): Promise<{ [key: string]: string } | undefined>{
+    const url = `${basicUrl}/${instrument}`;
+    try{
+      const response = await fetch(url);
+      if (response.ok){
+        const notesUrls = await response.json();
+        if (addPrefix){
+          return addPrefixUrls(notesUrls);
+        }
+        else{
+          return notesUrls;
+        } 
+      }
+    }
+    catch (err){
+      console.log(err)
+    }
+}
+
+function addPrefixUrls(urls: {[key: string]: string}): {[key: string]: string}{
+  const fullUrls: {[key: string]: string} = {}
+  const keys = Object.keys(urls);
+  keys.forEach((key) => {
+    const fullUrl = `${prefixUrl}/${urls[key]}`;
+    fullUrls[key] = fullUrl;
+  })
+  return fullUrls;
+}
